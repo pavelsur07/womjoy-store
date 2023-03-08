a6b9e507e2ec33882a64a8497047e6250f1595d0
pavel_novikov <pavelsur07@gmail.com>
2023-03-08 09:33:12 +0300

    Product Template setting

--- site/src/Controller/Store/ProductController.php
+++ site/src/Controller/Store/ProductController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route(path: '/product/{slug}')]
+    #[Route(path: '/product/{slug}', name: 'store.product.show')]
     public function show(string $slug): Response
     {
         return $this->render('store/product/show.html.twig');
