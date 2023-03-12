a39b7722fbc979451266858a8fccca889e8ed423
pavel_novikov <pavelsur07@gmail.com>
2023-03-12 12:41:19 +0300

    User test install

--- site/src/Controller/Store/InstallController.php
+++ site/src/Controller/Store/InstallController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\Store;
 
-use App\Entity\User\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,19 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class InstallController extends AbstractController
 {
-    #[Route(path: '/install/3038b048-1012-4a81-83a0-d3f800e08b2f/create-admin/', name: 'store.install')]
+    #[Route(path: '/install/install-admin/', name: 'store.install')]
     public function checkout(UserPasswordHasherInterface $hasher, UserRepository $users): Response
     {
-        $user = new User();
-        $user->setEmail('pavelsur07@gmail.com');
-        $hashed = $hasher->hashPassword(
-            user: $user,
-            plainPassword: 'passwordSecret'
-        );
-        $user->setRoles(['ROLE_ADMIN']);
-        $user->setPassword($hashed);
-        $users->save($user, true);
-
         return $this->redirectToRoute('admin.dashboard.show');
     }
 }
