9535e270df09ee38eadd113f6423f4577e1b429e
pavel_novikov <pavelsur07@gmail.com>
2023-04-25 10:45:16 +0300

    Symfony - update

--- site/src/Controller/HomeController.php
+++ site/src/Controller/HomeController.php
@@ -8,7 +8,6 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Intl\Locales;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
--- site/src/Controller/Store/ProductController.php
+++ site/src/Controller/Store/ProductController.php
@@ -10,8 +10,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    //#[Route(path: '/{_locale}/product/{slug}', name: 'store.product.show', requirements: ['_locale' => 'en|ru|bg'])]
-    #[Route(path: '/{_locale}/product/{slug}', name: 'store.product.show',requirements: ['_locale' => 'en|ru|bg'])]
+    // #[Route(path: '/{_locale}/product/{slug}', name: 'store.product.show', requirements: ['_locale' => 'en|ru|bg'])]
+    #[Route(path: '/{_locale}/product/{slug}', name: 'store.product.show', requirements: ['_locale' => 'en|ru|bg'])]
     public function show(string $slug): Response
     {
         return $this->render('store/product/show.html.twig');
