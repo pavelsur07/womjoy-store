2735cc69aaef8c8ff1a9066ced7768ce2cb1ca6b
pavel_novikov <pavelsur07@gmail.com>
2023-05-13 09:13:59 +0300

    [Store] Refactoring templates dashboard

--- site/src/Store/Infrastructure/Controller/Admin/DashboardController.php
+++ site/src/Store/Infrastructure/Controller/Admin/DashboardController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Store\Admin;
+namespace App\Store\Infrastructure\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,13 +13,6 @@ class DashboardController extends AbstractController
     #[Route('/admin/dashboard/', name: 'admin.dashboard.show', methods: ['GET'])]
     public function dashboard(): Response
     {
-        return $this->render('admin/dashboard/show.html.twig');
-    }
-
-    #[Route(path: '/admin/dashboard/tree-init', name: 'admin.tree_init')]
-    public function treeInit(): Response
-    {
-        $em = $this->getDoctrine()->getManager();
-        return $this->redirectToRoute('admin.dashboard.show');
+        return $this->render('store/admin/dashboard/show.html.twig');
     }
 }
