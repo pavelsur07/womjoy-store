e7cbc3bbbf2275e1e5e28ad1d81d1557a40493fc
pavel_novikov <pavelsur07@gmail.com>
2023-05-16 19:59:30 +0300

    [Store][Product] Refactoring show action

--- site/src/Store/Infrastructure/Controller/ProductController.php
+++ site/src/Store/Infrastructure/Controller/ProductController.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 
 namespace App\Store\Infrastructure\Controller;
 
+use App\Store\Infrastructure\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    // #[Route(path: '/{_locale}/product/{slug}', name: 'store.product.show', requirements: ['_locale' => 'en|ru|bg'])]
-    #[Route(path: '/product/{slug}', name: 'store.product.show', requirements: ['_locale' => 'en|ru|bg'])]
-    public function show(string $slug): Response
+    #[Route(path: '/product/{id}', name: 'store.product.show', requirements: ['_locale' => 'en|ru|bg'])]
+    public function show(int $id, ProductRepository $products): Response
     {
-        return $this->render('store/product/show.html.twig');
+        return $this->render(
+            'store/product/show.html.twig',
+            [
+                'product' => $products->get($id),
+            ]
+        );
     }
 }
