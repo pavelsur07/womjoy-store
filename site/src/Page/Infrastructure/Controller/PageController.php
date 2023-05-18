1a758d9c2c1de22780f32ee167252681cd37be94
pavel_novikov <pavelsur07@gmail.com>
2023-05-18 11:28:53 +0300

    Main menu refactoring

---
+++ site/src/Page/Infrastructure/Controller/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Page\Infrastructure\Controller;
+
+use App\Store\Infrastructure\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(path: '/pages', name: 'store.page')]
+class PageController extends AbstractController
+{
+    public const PER_PAGE= 15;
+
+    #[Route(path: '/pay-and-delivery', name: '.pay_delivery')]
+    public function payAndDelivery(Request $request, ProductRepository $products): Response
+    {
+        return $this->render(
+            'store/page/pay_delivery.html.twig',
+            []
+        );
+    }
+}
