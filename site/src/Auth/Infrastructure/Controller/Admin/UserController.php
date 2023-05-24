135f3cbd89addc29775e2a62a4ad2adbcfd6a552
pavel_novikov <pavelsur07@gmail.com>
2023-05-24 09:38:14 +0300

    [Auth] Refactoring roles #1

--- site/src/Auth/Infrastructure/Controller/Admin/UserController.php
+++ site/src/Auth/Infrastructure/Controller/Admin/UserController.php
@@ -14,8 +14,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/user')]
+#[IsGranted('ROLE_ADMIN')]
 class UserController extends AbstractController
 {
     #[Route('/', name: 'app.user.index', methods: ['GET'])]
