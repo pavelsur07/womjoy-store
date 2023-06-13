5c3cccec3ecca359ffc8df55d25b0360cb8b96fc
pavel_novikov <pavelsur07@gmail.com>
2023-06-13 06:47:03 +0300

    [Page] Refactoring Privacy

--- site/src/Store/Domain/Entity/Cart/Cart.php
+++ site/src/Store/Domain/Entity/Cart/Cart.php
@@ -4,7 +4,26 @@ declare(strict_types=1);
 
 namespace App\Store\Domain\Entity\Cart;
 
+use DateTimeImmutable;
+use Doctrine\Common\Collections\Collection;
+
 class Cart
 {
     private int $id;
+    private string $customerId;
+    private DateTimeImmutable $createdAt;
+    private DateTimeImmutable $updatedAt;
+    private string $status;
+    private Collection $items;
+
+    /**
+     * @param string            $customerId
+     * @param DateTimeImmutable $createdAt
+     */
+    public function __construct(string $customerId, DateTimeImmutable $createdAt)
+    {
+        $this->customerId = $customerId;
+        $this->createdAt = $createdAt;
+    }
+
 }
