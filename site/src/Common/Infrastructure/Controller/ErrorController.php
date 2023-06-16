5fce3484c5288c0976c0effcd4d2fefb6e9e8b8b
pavel_novikov <pavelsur07@gmail.com>
2023-06-16 16:16:19 +0300

    Site 404 page init - prod #1

---
+++ site/src/Common/Infrastructure/Controller/ErrorController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\Infrastructure\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ErrorController extends BaseController
+{
+    #[Route(path: '/_error/404', name: 'not.found')]
+    public function show(Request $request): Response
+    {
+        return $this->render(
+            'common/error/404.html.twig',
+            [
+                'metaData' => $this->metaData,
+                'menu' => $this->menu,
+            ]
+        );
+    }
+}
