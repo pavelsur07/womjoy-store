7f0b4064be68fff1098f611007e2c2cf654068f6
pavel_novikov <pavelsur07@gmail.com>
2023-08-10 07:50:33 +0300

    Template added blazy #1

--- site/assets/store.js
+++ site/assets/store.js
@@ -18,4 +18,25 @@ $(document).ready(function() {
 
 import Blazy from 'blazy'
 
-const bLazy = new Blazy()
+/*const bLazy = new Blazy()*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    const bLazy = new Blazy();
+
+    if (document.querySelectorAll('.product__img_slide').length > 0) {
+        document.querySelectorAll('.product__img_slide').forEach(imgSlide => {
+            const observer = new MutationObserver(mutations => {
+                mutations.forEach(mutation => {
+                    const img = mutation.target.querySelector('img');
+
+                    if (!img.classList.contains('b-loaded')) {
+                        bLazy.load(img, true);
+                    }
+                });
+            });
+            observer.observe(imgSlide, {
+                attributes: true,
+            });
+        });
+    }
+});
