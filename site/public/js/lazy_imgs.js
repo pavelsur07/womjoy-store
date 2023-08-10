9db5960274f928a5032dae871895b3681a2008f0
pavel_novikov <pavelsur07@gmail.com>
2023-08-10 07:44:34 +0300

    Template added blazy

---
+++ site/public/js/lazy_imgs.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const bLazy = new Blazy();
+
+	if (document.querySelectorAll('.product__img_slide').length > 0) {
+	    document.querySelectorAll('.product__img_slide').forEach(imgSlide => {
+	    	const observer = new MutationObserver(mutations => {
+		        mutations.forEach(mutation => {
+		        	const img = mutation.target.querySelector('img');
+
+		        	if (!img.classList.contains('b-loaded')) {
+			        	bLazy.load(img, true);
+		        	}
+		        });
+		    });
+	        observer.observe(imgSlide, {
+	        	attributes: true,
+	        });
+	    });
+	}
+});
