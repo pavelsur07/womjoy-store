2c61a97a52588e94f8f9f636096648aaa720e921
pavel_novikov <pavelsur07@gmail.com>
2023-09-02 06:03:02 +0300

    [Common] Refactoring ThumbnailService

--- site/src/Common/Infrastructure/Service/Thumbnail/ThumbnailService.php
+++ site/src/Common/Infrastructure/Service/Thumbnail/ThumbnailService.php
@@ -110,6 +110,82 @@ class ThumbnailService
 
     /**
      * @throws FilesystemException
+     * @throws ImageResizeException
+     */
+    public function createThumbnailImgproxy(
+        string $path,
+        string $inputName,
+        string $outputPath,
+        int $width = 0,
+        int $height = 0,
+        int $type = self::JPG,
+    ): File {
+        $fullName = $path . '/' . $inputName;
+
+        $tmp = tmpfile();
+        $file = $this->defaultStorage->read($fullName);
+        fwrite($tmp, $file);
+
+        $image = new ImageResize(stream_get_meta_data($tmp)['uri']);
+        // $image->resize($width, $height, true);
+
+        $image->crop($width, $height);
+        $stream = fopen('php://memory', 'wrb+');
+
+        $name = explode('.', $inputName)[0] . '.jpg';
+
+        switch ($type) {
+            case self::JPG:
+                $image->save($stream, IMAGETYPE_JPEG, self::QUALITY);
+                $this->defaultStorage->createDirectory($outputPath);
+                $this->defaultStorage->writeStream($outputPath . '/' . $name, $stream);
+                break;
+            case self::WEBP:
+                $image->save($stream, IMAGETYPE_WEBP, self::QUALITY);
+                $this->defaultStorage->createDirectory($outputPath);
+                $this->defaultStorage->writeStream($outputPath . '/' . $name . '.webp', $stream);
+        }
+
+        fclose($stream);
+        fclose($tmp);
+
+        $fileSize = $this->defaultStorage->fileSize($outputPath . '/' . $name);
+        return new File(
+            path: $path,
+            name: $name,
+            size: $fileSize
+        );
+    }
+
+    public function sign(string $str, $salt, $key): string
+    {
+        return hash_hmac('sha256', $salt . $str, $key, true);
+    }
+
+    public function saveImageFromURL($imageUrl, $savePath): false|string
+    {
+        // Получаем содержимое изображения по URL
+        $imageData = file_get_contents($imageUrl);
+
+        if ($imageData !== false) {
+            // Определяем расширение изображения на основе его MIME-типа
+            $imageType = mime_content_type($imageUrl);
+            $extension = image_type_to_extension(exif_imagetype($imageUrl));
+
+            // Формируем путь для сохранения файла
+            $filePath = $savePath . '/' . uniqid() . $extension;
+
+            // Сохраняем изображение
+            if (file_put_contents($filePath, $imageData)) {
+                return $filePath; // Возвращаем путь сохраненного файла
+            }
+            return false; // В случае ошибки возвращаем false
+        }
+        return false; // В случае ошибки загрузки изображения возвращаем false
+    }
+
+    /**
+     * @throws FilesystemException
      */
     public function remove(string $path, string $name): void
     {
