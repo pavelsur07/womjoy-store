0e0cc6fc1f3243b61360e0c357bf755f6175d834
pavel_novikov <pavelsur07@gmail.com>
2023-09-27 08:25:01 +0300

    [Store][Home] Added Widgets Entity

---
+++ site/src/Store/Domain/Entity/Home/ValueObject/WidgetNewArrivals.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Store\Domain\Entity\Home\ValueObject;
+
+class WidgetNewArrivals
+{
+    private bool $isActive = false;
+    private string $href = '';
+
+    public function isActive(): bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): void
+    {
+        $this->isActive = $isActive;
+    }
+
+    public function getHref(): string
+    {
+        return $this->href;
+    }
+
+    public function setHref(string $href): void
+    {
+        $this->href = $href;
+    }
+}
