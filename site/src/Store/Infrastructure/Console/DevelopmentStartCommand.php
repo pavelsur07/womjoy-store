0596d3414dc3eddd1ea5696d9721732eb1128c29
pavel_novikov <pavelsur07@gmail.com>
2023-11-25 08:55:10 +0300

    [Store] Added Init Ids Categories, Products

--- site/src/Store/Infrastructure/Console/DevelopmentStartCommand.php
+++ site/src/Store/Infrastructure/Console/DevelopmentStartCommand.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace App\Store\Infrastructure\Console;
 
+use App\Common\Infrastructure\Doctrine\Flusher;
+use App\Store\Domain\Entity\Category\Category;
+use App\Store\Domain\Entity\Product\Product;
+use App\Store\Domain\Repository\CategoryRepositoryInterface;
+use App\Store\Infrastructure\Repository\ProductRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -17,8 +22,30 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 class DevelopmentStartCommand extends Command
 {
+    public function __construct(
+        private readonly CategoryRepositoryInterface $categories,
+        private readonly ProductRepository $products,
+        private readonly Flusher $flusher,
+    ) {
+        parent::__construct();
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        // Генерация Ids категорий
+        /** @var Category $item */
+        foreach ($this->categories->getAll() as $item) {
+            $item->generateIds();
+            $this->flusher->flush();
+        }
+
+        // Генерация Ids товаров
+        /** @var Product $item */
+        foreach ($this->products->getAllIterator() as $item) {
+            $item->setCategoriesIds();
+            $this->flusher->flush();
+        }
+
         return Command::SUCCESS;
     }
 }
