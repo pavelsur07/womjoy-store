521fd34732da0dff4c05be81b3f201989b4a41d9
pavel_novikov <pavelsur07@gmail.com>
2023-11-25 09:50:22 +0300

    Update template Pion

--- site/src/Store/Infrastructure/Controller/Admin/Order/AmoCRMController.php
+++ site/src/Store/Infrastructure/Controller/Admin/Order/AmoCRMController.php
@@ -9,10 +9,7 @@ use AmoCRM\Collections\CustomFieldsValuesCollection;
 use AmoCRM\Collections\TagsCollection;
 use AmoCRM\Exceptions\AmoCRMApiException;
 use AmoCRM\Models\ContactModel;
-use AmoCRM\Models\CustomFieldsValues\MultitextCustomFieldValuesModel;
 use AmoCRM\Models\CustomFieldsValues\TextCustomFieldValuesModel;
-use AmoCRM\Models\CustomFieldsValues\ValueCollections\MultitextCustomFieldValueCollection;
-use AmoCRM\Models\CustomFieldsValues\ValueModels\MultitextCustomFieldValueModel;
 use AmoCRM\Models\LeadModel;
 use AmoCRM\Models\TagModel;
 use App\Store\Domain\Entity\Order\ValueObject\OrderId;
@@ -36,20 +33,17 @@ class AmoCRMController extends AbstractController
             $order = $orders->get(new OrderId($orderId));
 
             $leadsService = $apiClient->leads();
-            /*$lead = new LeadModel();*/
-
+            // $lead = new LeadModel();
 
             $contact = new ContactModel();
             $contact->setFirstName($order->getCustomer()->getFirstName());
             $contact->setLastName($order->getCustomer()->getLastName());
-
 
             $contactCollection = (new ContactsCollection())->add($contact);
 
             $fieldsCollection = new CustomFieldsValuesCollection();
 
             $fields = new TextCustomFieldValuesModel();
-
 
             $lead = (new LeadModel())
                 ->setName('Заказ ' . $order->getCustomer()->getName())
@@ -63,9 +57,8 @@ class AmoCRMController extends AbstractController
                 )
                 ->setCreatedAt($order->getCreatedAt()->getTimestamp())
                 ->setContacts($contactCollection)
-               /* ->setCustomFieldsValues($fieldsCollection)*/
+               // ->setCustomFieldsValues($fieldsCollection)
                 ->setRequestId($order->getId()->value());
-
 
             $lead = $leadsService->addOne($lead);
 
