4b128a30d643104cea2da3d7feb2d3de11c3da46
pavel_novikov <pavelsur07@gmail.com>
2023-11-26 09:53:07 +0300

    [Store][Order] Create lead in AmoCRM #1

--- site/src/Store/Infrastructure/Controller/Admin/Order/AmoCRMController.php
+++ site/src/Store/Infrastructure/Controller/Admin/Order/AmoCRMController.php
@@ -9,7 +9,12 @@ use AmoCRM\Collections\CustomFieldsValuesCollection;
 use AmoCRM\Collections\TagsCollection;
 use AmoCRM\Exceptions\AmoCRMApiException;
 use AmoCRM\Models\ContactModel;
+use AmoCRM\Models\CustomFieldsValues\MultitextCustomFieldValuesModel;
 use AmoCRM\Models\CustomFieldsValues\TextCustomFieldValuesModel;
+use AmoCRM\Models\CustomFieldsValues\ValueCollections\MultitextCustomFieldValueCollection;
+use AmoCRM\Models\CustomFieldsValues\ValueCollections\TextCustomFieldValueCollection;
+use AmoCRM\Models\CustomFieldsValues\ValueModels\MultitextCustomFieldValueModel;
+use AmoCRM\Models\CustomFieldsValues\ValueModels\TextCustomFieldValueModel;
 use AmoCRM\Models\LeadModel;
 use AmoCRM\Models\TagModel;
 use App\Store\Domain\Entity\Order\ValueObject\OrderId;
@@ -36,14 +41,15 @@ class AmoCRMController extends AbstractController
             // $lead = new LeadModel();
 
             $contact = new ContactModel();
+            $contact->setName($order->getCustomer()->getFirstName(). ' '.$order->getCustomer()->getLastName());
             $contact->setFirstName($order->getCustomer()->getFirstName());
             $contact->setLastName($order->getCustomer()->getLastName());
 
-            $contactCollection = (new ContactsCollection())->add($contact);
+            // Добавляем телефон и email в контакт
+            //$contactFields = new CustomFieldsValuesCollection();
 
-            $fieldsCollection = new CustomFieldsValuesCollection();
+            //$contact->setCustomFieldsValues($contactFields);
 
-            $fields = new TextCustomFieldValuesModel();
 
             $lead = (new LeadModel())
                 ->setName('Заказ ' . $order->getCustomer()->getName())
@@ -56,15 +62,87 @@ class AmoCRMController extends AbstractController
                         )
                 )
                 ->setCreatedAt($order->getCreatedAt()->getTimestamp())
-                ->setContacts($contactCollection)
-               // ->setCustomFieldsValues($fieldsCollection)
-                ->setRequestId($order->getId()->value());
+                ->setSourceExternalId($order->getId()->value());
+
+            //$lead->setContacts((new ContactsCollection())->add($contact));
+
+            $lead->setContacts(
+                (new ContactsCollection())
+                    ->add(
+                        (new ContactModel())
+                            ->setFirstName('first_name')
+                            ->setLastName('last_name')
+                            ->setCustomFieldsValues(
+                                (new CustomFieldsValuesCollection())
+                                    ->add(
+                                        (new MultitextCustomFieldValuesModel())
+                                            ->setFieldCode('ТЕЛЕФОН')
+                                            ->setValues(
+                                                (new MultitextCustomFieldValueCollection())
+                                                    ->add(
+                                                        (new MultitextCustomFieldValueModel())
+                                                            ->setValue('+79888')
+                                                    )
+                                            )
+                                    )
+                            )
+                    )
+            );
+
+            //-------------------------------------------------------------------------------------
+            // Создадим коллекцию полей сущности
+            $fieldsValues = new CustomFieldsValuesCollection();
+            // Создадим модель значений поля типа текст
+            $textCustomFieldValuesModel = new TextCustomFieldValuesModel();
+            // Укажем ID поля
+            $textCustomFieldValuesModel->setFieldId(1001239);
+            // Добавим значения
+            $textCustomFieldValuesModel->setValues(
+                (new TextCustomFieldValueCollection())
+                    ->add((new TextCustomFieldValueModel())->setValue('Текст'))
+            );
+            // Добавим значение в коллекцию полей сущности
+            $fieldsValues->add($textCustomFieldValuesModel);
+
+            // Создадим модель значений поля типа текст
+            $numberField = new TextCustomFieldValuesModel();
+            // Укажем ID поля
+            $numberField->setFieldId(670825);
+            $numberField->setValues(
+                (new TextCustomFieldValueCollection())
+                    ->add((new TextCustomFieldValueModel())->setValue((string)$order->getOrderNumber()->value()))
+            );
+
+            // Добавим значение в коллекцию полей сущности
+            $fieldsValues->add($numberField);
+
+            // _ym_uid
+            $utmContentField = new TextCustomFieldValuesModel();
+            $utmContentField->setFieldCode('_YM_UID');
+            $utmContentField->setValues(
+                (new TextCustomFieldValueCollection())
+                    ->add((new TextCustomFieldValueModel())->setValue($order->getYmUid()))
+            );
+            $fieldsValues->add($utmContentField);
+
+            // _ym_counter
+            $utmContentField = new TextCustomFieldValuesModel();
+            $utmContentField->setFieldCode('_YM_COUNTER');
+            $utmContentField->setValues(
+                (new TextCustomFieldValueCollection())
+                    ->add((new TextCustomFieldValueModel())->setValue($order->getYmCounter()))
+            );
+
+            $fieldsValues->add($utmContentField);
+
+            // Установим в сущности эти поля
+            $lead->setCustomFieldsValues($fieldsValues);
 
             $lead = $leadsService->addOne($lead);
 
             $this->addFlash('success', 'Success Test lead is added. ' . $lead->getPrice());
         } catch (AmoCRMApiException $e) {
-            $this->addFlash('success', 'Error lead is not added.');
+            $this->addFlash('danger', 'Error lead is not added.');
         }
 
         return $this->redirectToRoute('store.order.admin.show', ['id'=> $orderId]);
