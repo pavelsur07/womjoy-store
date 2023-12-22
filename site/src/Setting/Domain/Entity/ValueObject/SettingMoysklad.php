882beb6de8218dfd9c6cc6b4a946c3b554d60681
pavel_novikov <pavelsur07@gmail.com>
2023-12-22 08:32:41 +0300

    [Setting] Added Unisender, Moysklad

---
+++ site/src/Setting/Domain/Entity/ValueObject/SettingMoysklad.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Setting\Domain\Entity\ValueObject;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Embeddable]
+class SettingMoysklad
+{
+}
---
+++ site/src/Setting/Domain/Entity/ValueObject/SettingUnisender.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Setting\Domain\Entity\ValueObject;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Embeddable]
+class SettingUnisender
+{
+    private ?string $key = null;
+
+    public function __construct(?string $key)
+    {
+        $this->key = $key;
+    }
+
+    public function getKey(): ?string
+    {
+        return $this->key;
+    }
+}
