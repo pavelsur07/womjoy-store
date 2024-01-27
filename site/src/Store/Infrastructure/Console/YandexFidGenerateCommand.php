3156527a223995a843fd5473e761aa687582e2ff
pavel_novikov <pavelsur07@gmail.com>
2024-01-27 10:35:31 +0300

    [Store][Yml] Added YandexFidGenerateCommand

---
+++ site/src/Store/Infrastructure/Console/YandexFidGenerateCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Store\Infrastructure\Console;
+
+use App\Store\Domain\Entity\Yml\Item;
+use App\Store\Domain\Entity\Yml\Yml;
+use App\Store\Infrastructure\Repository\YmlRepository;
+use App\Store\Infrastructure\Service\YandexMarket\YandexMarketGenerator;
+use League\Flysystem\FilesystemException;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'yandex:fid-generate',
+    description: 'Yandex fid generate.',
+    aliases: ['yandex:fid-generate'],
+    hidden: false
+)]
+class YandexFidGenerateCommand extends Command
+{
+    public function __construct(
+        private readonly YmlRepository $ymls,
+        private readonly YandexMarketGenerator $generator,
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * @throws FilesystemException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $this->ymls->list();
+        $items = [];
+
+        /** @var Yml $value */
+        foreach ($this->ymls->list() as $value) {
+            $items[] = $value->getId();
+        }
+
+        $this->generator->setProperty(
+            company: 'ИП Новикова Н.А.',
+            name: 'Womjoy',
+            url: 'https://womjoy.ru'
+        );
+
+        foreach ($items as $item) {
+            $fid = $this->ymls->get($item);
+
+            $categories = [];
+            $products =[];
+
+            /** @var Item $value */
+            foreach ($fid->getItems() as $value) {
+                $categories[$value->getProduct()->getMainCategory()->getId()] = $value->getProduct()->getMainCategory();
+                if ($value->getProduct()->getStatus()->isActive()) {
+                    $products[] = $value->getProduct();
+                }
+            }
+
+            $this->generator->generate($categories, $products, $fid->getFileName());
+        }
+
+        return Command::SUCCESS;
+    }
+}
