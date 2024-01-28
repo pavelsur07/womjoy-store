c0f75af3ba84ae3b1382a233786a80ded790188a
pavel_novikov <pavelsur07@gmail.com>
2024-01-28 09:42:31 +0300

    [Store][Message] Added ReCaptcha

--- site/src/Store/Infrastructure/Controller/Store/MessageController.php
+++ site/src/Store/Infrastructure/Controller/Store/MessageController.php
@@ -12,6 +12,7 @@ use App\Store\Domain\Entity\Message\ValueObject\MessageTopic;
 use App\Store\Infrastructure\Form\Message\MessageNewForm;
 use App\Store\Infrastructure\Repository\MessageRepository;
 use DateTimeImmutable;
+use ReCaptcha\ReCaptcha;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,8 +20,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class MessageController extends BaseController
 {
     #[Route(path: '/about/contact/', name: 'store.message.contact.new')]
-    public function message(Request $request, Flusher $flusher, MessageRepository $messages): Response
-    {
+    public function message(
+        Request $request,
+        Flusher $flusher,
+        MessageRepository $messages,
+        string $googleRecaptchaSiteKey,
+        string $googleRecaptchaSecret
+    ): Response {
+        $recaptcha = new ReCaptcha($googleRecaptchaSecret);
+        $resp = $recaptcha->verify($request->request->get('g-recaptcha-response'), $request->getClientIp());
+
         $messageId = $request->get('messageId');
         if ($messageId === null) {
             $message = null;
@@ -32,6 +41,13 @@ class MessageController extends BaseController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            if (!$resp->isSuccess()) {
+                // Do something if the submit wasn't valid ! Use the message to show something
+                $message = "The reCAPTCHA wasn't entered correctly. Go back and try it again.";
+                $this->addFlash('danger', $message);
+                return $this->redirectToRoute('store.message.contact.new', ['messageId' =>'error']);
+            }
+
             $data = $form->getData();
             $newMessage = new Message(
                 id: MessageId::generate(),
@@ -44,7 +60,7 @@ class MessageController extends BaseController
             );
             $messages->save($newMessage);
             $flusher->flush();
-
+            $this->addFlash('success', 'Message sending success.');
             return $this->redirectToRoute('store.message.contact.new', ['messageId' =>'success']);
         }
         return $this->render(
@@ -54,6 +70,7 @@ class MessageController extends BaseController
                 'menu' => $this->menu,
                 'form' => $form->createView(),
                 'messageId' => $message,
+                'site_key' => $googleRecaptchaSiteKey,
             ]
         );
     }
