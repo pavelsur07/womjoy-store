c08fbf85c60428ede1ce2e9055d4525b44dbeb49
pavel_novikov <pavelsur07@gmail.com>
2024-02-04 10:44:52 +0300

    [Store][Product] SubscribeProduct added property createdAt

--- site/src/Store/Domain/Entity/Product/SubscribeProduct.php
+++ site/src/Store/Domain/Entity/Product/SubscribeProduct.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Store\Domain\Entity\Product;
 
 use App\Store\Domain\Entity\Product\ValueObject\SubscribeProductId;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use Webmozart\Assert\Assert;
 
@@ -21,12 +22,21 @@ class SubscribeProduct
     #[ORM\Column(type: 'string', length: 60)]
     private string $email;
 
-    public function __construct(SubscribeProductId $id, Variant $variant, string $email)
+    #[ORM\Column(type: 'datetime_immutable')]
+    private DateTimeImmutable $createdAt;
+
+    public function __construct(SubscribeProductId $id, Variant $variant, string $email, DateTimeImmutable $createdAt)
     {
         Assert::email($email);
         $this->id = $id;
         $this->variant = $variant;
         $this->email = $email;
+        $this->createdAt = $createdAt;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
     }
 
     public function getId(): SubscribeProductId
