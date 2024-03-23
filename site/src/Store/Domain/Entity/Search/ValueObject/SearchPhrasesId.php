fb5e9a2e0c27d9c4c3517541169edb81b42d40e9
pavel_novikov <pavelsur07@gmail.com>
2024-03-23 10:59:48 +0300

    [Store][Search]

---
+++ site/src/Store/Domain/Entity/Search/ValueObject/SearchPhrasesId.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Store\Domain\Entity\Search\ValueObject;
+
+use App\Common\Domain\Entity\ValueObject\UuidValueObject;
+
+class SearchPhrasesId extends UuidValueObject {}
---
+++ site/src/Store/Infrastructure/Controller/Store/SearchController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Store\Infrastructure\Controller\Store;
+
+use App\Common\Infrastructure\Controller\BaseController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends BaseController
+{
+    #[Route(path: '/search', name: 'search' )]
+    public function search(Request $request): Response
+    {
+        return $this->render("{$this->template}/store/search/index.html.twig",
+            [
+                'metaData' => $this->metaData,
+                'menu' => $this->menu,
+                'referer' => $request->headers->get('referer'),
+                'param'=> $request->get('param'),
+            ]);
+    }
+}
