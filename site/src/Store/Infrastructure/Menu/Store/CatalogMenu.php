9a9b207f2de1d6d9481f1944ee2b61f36b36fa68
pavel_novikov <pavelsur07@gmail.com>
2024-04-14 15:35:48 +0300

    [Site] Add CatalogMenu #1

--- site/src/Store/Infrastructure/Menu/Store/CatalogMenu.php
+++ site/src/Store/Infrastructure/Menu/Store/CatalogMenu.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Store\Infrastructure\Menu\Store;
 
 class CatalogMenu
 {
-
     public function build(): array
     {
         return [
@@ -16,15 +17,15 @@ class CatalogMenu
                 'items'=> [
                     [
                         'name' => 'НОВИНКИ',
-                        'href' => 'https://womjoy.ru/collections/novinki-dlya-zhenshchin-115'
+                        'href' => 'https://womjoy.ru/collections/novinki-dlya-zhenshchin-115',
                     ],
                     [
                         'name' => 'СКОРО В ПРОДАЖЕ',
-                        'href' => 'https://womjoy.ru/collections/skoro-v-prodazhe-118'
-                    ]
-                ]
+                        'href' => 'https://womjoy.ru/collections/skoro-v-prodazhe-118',
+                    ],
+                ],
             ],
-            /* -  Меню с вложенными элементами - */
+            // -  Меню с вложенными элементами -
             [
                 'name'=>'Каталог',
                 'href' =>'https://womjoy.ru/collections/zhenskie-bryuki-108',
@@ -33,51 +34,51 @@ class CatalogMenu
                 'items'=> [
                     [
                         'name' => 'Брюки',
-                        'href' => 'https://womjoy.ru/collections/zhenskie-bryuki-108'
+                        'href' => 'https://womjoy.ru/collections/zhenskie-bryuki-108',
                     ],
                     [
                         'name' => 'Велосипедки',
-                        'href' => 'https://womjoy.ru/collections/velosipedki-zhenskie-113'
+                        'href' => 'https://womjoy.ru/collections/velosipedki-zhenskie-113',
                     ],
                     [
                         'name' => 'Лонгсливы',
-                        'href' => 'https://womjoy.ru/collections/zhenskie-vodolazki-i-longslivy-142'
+                        'href' => 'https://womjoy.ru/collections/zhenskie-vodolazki-i-longslivy-142',
                     ],
                     [
                         'name' => 'Легинсы',
-                        'href' => 'https://womjoy.ru/collections/zhenskie-leginsy-i-losiny-109'
+                        'href' => 'https://womjoy.ru/collections/zhenskie-leginsy-i-losiny-109',
                     ],
                     [
                         'name' => 'Майки',
-                        'href' => 'https://womjoy.ru/collections/zhenskie-mayki-135'
+                        'href' => 'https://womjoy.ru/collections/zhenskie-mayki-135',
                     ],
                     [
                         'name' => 'Халаты',
-                        'href' => 'https://womjoy.ru/collections/halaty-zhenskie'
+                        'href' => 'https://womjoy.ru/collections/halaty-zhenskie',
                     ],
                     [
                         'name' => 'Рубашки',
-                        'href' => 'https://womjoy.ru/collections/rubashki-zhenskie-143'
+                        'href' => 'https://womjoy.ru/collections/rubashki-zhenskie-143',
                     ],
                     [
                         'name' => 'Толстовки и свитшоты',
-                        'href' => 'https://womjoy.ru/collections/zhenskie-tolstovki-i-svitshot-139'
+                        'href' => 'https://womjoy.ru/collections/zhenskie-tolstovki-i-svitshot-139',
                     ],
                     [
                         'name' => 'Топы и кроп-топы',
-                        'href' => 'https://womjoy.ru/collections/zhenskie-topy-136'
+                        'href' => 'https://womjoy.ru/collections/zhenskie-topy-136',
                     ],
                     [
                         'name' => 'Футболки',
-                        'href' => 'https://womjoy.ru/collections/zhenskie-futbolki-134'
+                        'href' => 'https://womjoy.ru/collections/zhenskie-futbolki-134',
                     ],
                     [
                         'name' => 'Шорты',
-                        'href' => 'https://womjoy.ru/collections/zhenskie-shorty-114'
+                        'href' => 'https://womjoy.ru/collections/zhenskie-shorty-114',
                     ],
-                ]
+                ],
             ],
-            /* -  Меню список - */
+            // -  Меню список -
             [
                 'name'=>'НОВИНКИ',
                 'href' =>'https://womjoy.ru/collections/novinki-dlya-zhenshchin-115',
@@ -86,23 +87,22 @@ class CatalogMenu
                 'items'=> [
                     [
                         'name' => 'ОДЕЖДА ДЛЯ ДОМА',
-                        'href' => 'https://womjoy.ru/collections/odezhda-dlya-doma-121'
+                        'href' => 'https://womjoy.ru/collections/odezhda-dlya-doma-121',
                     ],
                     [
                         'name' => 'ОДЕЖДА ДЛЯ СПОРТА',
-                        'href' => 'https://womjoy.ru/collections/odezhda-dlya-sporta-120'
+                        'href' => 'https://womjoy.ru/collections/odezhda-dlya-sporta-120',
                     ],
                     [
                         'name' => 'Одежда для путешествий',
-                        'href' => 'https://womjoy.ru/collections/odezhda-dlya-aktivnogo-otdyha'
+                        'href' => 'https://womjoy.ru/collections/odezhda-dlya-aktivnogo-otdyha',
                     ],
                     [
                         'name' => 'АКСЕССУАРЫ',
-                        'href' => 'https://womjoy.ru/collections/odezhda-dlya-aktivnogo-otdyha'
+                        'href' => 'https://womjoy.ru/collections/odezhda-dlya-aktivnogo-otdyha',
                     ],
-                ]
+                ],
             ],
-
         ];
     }
 }
