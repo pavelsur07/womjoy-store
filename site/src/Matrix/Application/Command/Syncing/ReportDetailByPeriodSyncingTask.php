3f9a94b86b71c5e7dd86598ce26bbf0bef9f48fe
pavel_novikov <pavelsur07@gmail.com>
2024-04-30 19:48:25 +0300

    [Matrix] - Remove

--- site/src/Matrix/Application/Command/Syncing/ReportDetailByPeriodSyncingTask.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Matrix\Application\Command\Syncing;
-
-class ReportDetailByPeriodSyncingTask {}
--- site/src/Matrix/Infrastructure/Wildberries/Exception/InternalServerErrorException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Matrix\Infrastructure\Wildberries\Exception;
-
-final class InternalServerErrorException extends WildberriesApiException {}
