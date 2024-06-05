ea4f2f921781d8ed318ecd6ad8a8c89448df4fd9
pavel_novikov <pavelsur07@gmail.com>
2024-06-05 15:53:25 +0300

    [Site][Cart] Add Assert email

--- site/src/Store/Domain/Entity/Cart/ValueObject/CartCustomer.php
+++ site/src/Store/Domain/Entity/Cart/ValueObject/CartCustomer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Store\Domain\Entity\Cart\ValueObject;
 
 use Doctrine\ORM\Mapping as ORM;
+use Webmozart\Assert\Assert;
 
 #[ORM\Embeddable]
 class CartCustomer
@@ -25,6 +26,7 @@ class CartCustomer
 
     public function setEmail(?string $email): void
     {
+        Assert::email($email);
         $this->email = $email;
     }
 
